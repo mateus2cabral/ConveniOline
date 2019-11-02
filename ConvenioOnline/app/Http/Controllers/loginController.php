33ce56511a6ce0b01a5acfe8caf697391df2298c
DinoDbene <denilsonm604@gmail.com>
2019-11-02 16:04:30 -0300

    Foram adicionados manualmente no banco de dados os usuarios: {preg123, 1234} e {empr123, 1234} / Consegui fazer a validação na hora de logar conferindo se o login inserido esta presente na base de dados / conforme o login inserido o usuario é redirecionado para sua respectiva tela de inicio

--- ConvenioOnline/app/Http/Controllers/loginController.php
+++ ConvenioOnline/app/Http/Controllers/loginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class loginController extends Controller
 {
@@ -14,21 +15,29 @@ class loginController extends Controller
         $usuario = $dados->usuario;
         $senha = $dados->senha;
         
+        $users = DB::collection('logins')->get();
 
         if ($usuario != null && $senha != null) {
 
-            if (stristr($usuario, 'preg')) {
+            foreach ($users as $user) {
+                if($user["login"] === $usuario && $user["senha"] === $senha) {
+                    
+                    if (stristr($usuario, 'preg')) {
                 
-                return view('preg.inicio_preg');
-
-            } else if (stristr($usuario, 'empr')) {
-                
-                return view('empresa.inicio_empresa');
-
-            }
-
-            else {return view('login');}
+                        return view('preg.inicio_preg');
         
+                    } else if (stristr($usuario, 'empr')) {
+                        
+                        return view('empresa.inicio_empresa');
+        
+                    } 
+
+                } else {
+                    return view('login');
+                }
+                  
+            } 
+
         } else {
             return view('login');
         }
