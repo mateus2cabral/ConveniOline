40192b080e0bd5818b61c1de3ad84b8f47871a40
DinoDbene <denilsonm604@gmail.com>
2019-12-04 11:25:06 -0300

    Estrutura de abas para o professor indicar um aluno para um estío: ok

--- ConvenioOnline/app/Http/Controllers/nova_indicacaoController.php
+++ ConvenioOnline/app/Http/Controllers/nova_indicacaoController.php
@@ -9,8 +9,34 @@ class nova_indicacaoController extends Controller
 {
 
     public function show() {
-    
-        return view('professor.nova_indicacao');
+
+        // filtra as empresas e recolhe as areas disponiveis em umm vetor
+
+        $aconhecimentos = DB::collection('aconhecimentos')->get();
+        $areas = [];
+
+        foreach ($aconhecimentos as $area) {
+            if (sizeof($areas) === 0) {
+                array_push($areas, $area['area']);
+
+            } else {
+                $repetido = false;
+
+                foreach ($areas as $a) {
+
+                    if($area['area'] === $a){
+                        $repetido = true;
+                    }
+                }
+
+                if(!$repetido) {
+                    array_push($areas, $area['area']);
+                }
+                
+            }
+        }
+        
+        return view('professor.nova_indicacao', compact('areas'));
     }
 
 }
