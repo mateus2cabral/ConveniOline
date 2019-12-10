c284afdab602f13cc1381b37eafab73568c776c0
jmateuscabral <jmateuscabral@gmail.com>
2019-12-09 21:08:44 -0300

    Ajustes das datas da frequência dos estagiários

--- ConvenioOnline/app/Http/Controllers/estagiariosController.php
+++ ConvenioOnline/app/Http/Controllers/estagiariosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DateTime;
 use DB;
 
 class estagiariosController extends Controller
@@ -37,17 +38,90 @@ class estagiariosController extends Controller
         // echo $id;    
         
         $estagiarios = DB::collection('alunos')->get();
-        
+        $data_atual = new DateTime();
+
+        echo $data_atual->format('m');
+
+        $segunda = date_create($data_atual->format('d/m/Y'));
+        $terca = date_create($data_atual->format('d/m/Y'));
+        $quarta = date_create($data_atual->format('d/m/Y'));
+        $quinta = date_create($data_atual->format('d/m/Y'));
+        $sexta = date_create($data_atual->format('d/m/Y'));
+
+
+        switch ($data_atual->format('l')) {
+            case 'Monday':
+                $segunda = new DateTime();
+                $terca = new DateTime('+1 day');
+                $quarta = new DateTime('+2 day');
+                $quinta = new DateTime('+3 day');
+                $sexta = new DateTime('+4 day');
+                break;
+
+            case 'Tuesday':
+                $segunda = new DateTime('-1 day');
+                $terca = new DateTime();
+                $quarta = new DateTime('+1 day');
+                $quinta = new DateTime('+2 day');
+                $sexta = new DateTime('+3 day');
+                break;
+
+            case 'Wednesday':
+                $segunda = new DateTime('-2 day');
+                $terca = new DateTime('-1 day');
+                $quarta = new DateTime();
+                $quinta = new DateTime('+1 day');
+                $sexta = new DateTime('+2 day');
+                break;
+                
+            case 'Thursday':
+                $segunda = new DateTime('-3 day');
+                $terca = new DateTime('-2 day');
+                $quarta = new DateTime('-1 day');
+                $quinta = new DateTime();
+                $sexta = new DateTime('-4 day');
+                break;
+
+            case 'Friday':
+                $segunda = new DateTime('-4 day');
+                $terca = new DateTime('-3 day');
+                $quarta = new DateTime('-2 day');
+                $quinta = new DateTime('-1 day');
+                $sexta = new DateTime();
+                break;
+
+            case 'Saturday':
+                $segunda = new DateTime('-5 day');
+                $terca = new DateTime('-4 day');
+                $quarta = new DateTime('-3 day');
+                $quinta = new DateTime('-2 day');
+                $sexta = new DateTime('-1 day');
+                break;
+
+            case 'Sunday':
+                $segunda = new DateTime('-6 day');
+                $terca = new DateTime('-5 day');
+                $quarta = new DateTime('-4 day');
+                $quinta = new DateTime('-3 day');
+                $sexta = new DateTime('-2 day');
+                break;
+
+        }
+
+        $datas = array (
+            "segunda" => $segunda->format('d/m/Y'),
+            "terca" => $terca->format('d/m/Y'),
+            "quarta" => $quarta->format('d/m/Y'),
+            "quinta" => $quinta->format('d/m/Y'),
+            "sexta" => $sexta->format('d/m/Y')
+        );       
         
         foreach ($estagiarios as $estagiario) {
-            // echo $estagiario['nome'];
             if ($estagiario['_id'] == $id) {
-                // echo $estagiario['nome'];
-                return view('supervisor.frequencia', compact('estagiario'));
+                return view('supervisor.frequencia', compact('estagiario'), compact('datas'));
             }
         }
 
-        // echo $estagiario;
     }
 
 
