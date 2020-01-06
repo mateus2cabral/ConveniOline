bf1b40f10e8bd809b53017b8aad8dcc2556b93e4
jmateuscabral <jmateuscabral@gmail.com>
2020-01-06 17:48:56 -0300

    Ajustes em professor

--- ConvenioOnline/resources/views/professor/minhas_indicacoes.blade.php
+++ ConvenioOnline/resources/views/professor/minhas_indicacoes.blade.php
@@ -16,20 +16,19 @@
 
                         <tr>
                             <th>Aluno</th>
-                            <th>Matricula</th>
+                            <!-- <th>Matricula</th> -->
                             <th>Empresa</th>
                             <th>Área</th>
                             <th>Data</th>
                             <th>Status</th>
-                            <th>Plano de estagio</th>
+                            <th>Plano</th>
                             <th>Frequencia</th>
                         </tr>
                         
                         @foreach ($meusEstagios as $estagio)
-<<<<<<< HEAD
                             <tr align="center">
                             <td>{{ $estagio['nomeAluno'] }}</td>
-                            <td>{{ $estagio['matriculaAluno'] }}</td>
+                            <!-- <td>{{ $estagio['matriculaAluno'] }}</td> -->
                             <td>{{ $estagio['empresa'] }}</td>
                             <td>{{ $estagio['area'] }}</td>
                             <td>{{ $estagio['data'] }}</td>
@@ -48,40 +47,13 @@
                             @if ( $estagio['status'] === 're')
                                 <td>Rejeitado pela empresa</td>
                             @endif
-                        <td>
-                            <a href="plano_estagio"><button class="button-plano">Analisar</button></a>
-                        </td>
-                        <td>
-                            <a><button class="button-plano">Verificar</button>
-                        </td>
-                            
-=======
-                            <a href="inicio_professor">
-
-                                <tr class="table-row" align="center">
-                                    <td>{{ $estagio['nomeAluno'] }}</td>
-                                    <td>{{ $estagio['matriculaAluno'] }}</td>
-                                    <td>{{ $estagio['empresa'] }}</td>
-                                    <td>{{ $estagio['area'] }}</td>
-                                    <td>{{ $estagio['data'] }}</td>
-                                    @if ( $estagio['status'] === 'ae')
-                                    <td>Aguardando analise da empresa</td>
-                                    @endif
-                                    @if ( $estagio['status'] === 'ap')
-                                    <td>Aguardando analise da PREG</td>
-                                    @endif
-                                    @if ( $estagio['status'] === 'a')
-                                    <td>Aprovado</td>
-                                    @endif
-                                    @if ( $estagio['status'] === 'rp')
-                                    <td>Rejeitado pela PREG</td>
-                                    @endif
-                                    @if ( $estagio['status'] === 're')
-                                    <td>Rejeitado pela empresa</td>
-                                    @endif
-                                </tr>
-                            </a>
->>>>>>> 925baa2d48fe9c091f10251d696ab7792936b1b2
+                            <td>
+                                <a href="inicio_professor"><button class="button-plano">Analisar</button></a>
+                            </td>
+                            <td>
+                                <a href="inicio_professor"><button class="button-plano">Verificar</button></a>
+                            </td>
+                                
                         @endforeach  
                     </table>
                 
