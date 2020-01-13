04eef11e7fa9c323e0fa305f942527156793a123
DinoDbene <denilsonm604@gmail.com>
2020-01-13 15:54:08 -0300

    botões de frequencia e plano de estagio adicionados na view de professor->minhas indicações e na view supervisor->supervisionar

---
+++ ConvenioOnline/resources/views/professor/frequencia_estagio_not_found.blade.php
@@ -0,0 +1,12 @@
+@extends('templates.moldura_professor')
+@section('content')
+
+    <link rel='stylesheet' type='text/css' media='screen' href='./css/professor/frequencia_estagio_not_found.css'>
+
+    <article class="feed">
+        <div class="feed-msg">
+            Aluno ainda não possui uma frequencia de estágio!
+        </div>
+    </article>
+    
+@endsection
---
+++ ConvenioOnline/resources/views/supervisor/plano.blade.php
@@ -0,0 +1,12 @@
+@extends('templates.moldura_professor')
+@section('content')
+
+    <link rel='stylesheet' type='text/css' media='screen' href='./css/professor/plano_estagio_not_found.css'>
+
+    <article class="feed">
+        <div class="feed-msg">
+            Plano de estagio do aluno!
+        </div>
+    </article>
+    
+@endsection
